0c2cbe20ede71458e4c1e3f46e64ee18ecd5513c
Fabio Calero <fabiojcalero@gmail.com>
2024-07-19 06:21:12 -0400

    Update gatsby-config.js to include link to most recent image

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,54 +1,54 @@
 module.exports = {
-  pathPrefix: "/tinydesk",
-  siteMetadata: {
-    title: `Tiny Desk`,
-    description: `Productivity tool that lets power web users manage bookmarks in a sensible way and increase their productivity online. With Tiny Desk, you can manage bookmarks, create to-do lists, and write notes. Start using Tiny Desk and boost your web browsing productivity today.`,
-    author: `Fabio Calero and Team KGB`,
-    viewport: "width=device-width, initial-scale=1, shrink-to-fit=no",
-    image:
-      "https://user-images.githubusercontent.com/31612210/113110528-24555100-9242-11eb-9725-a4f8b43fa5d2.png",
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+    pathPrefix: "/tinydesk",
+    siteMetadata: {
+        title: `Tiny Desk`,
+        description: `Productivity tool that lets power web users manage bookmarks in a sensible way and increase their productivity online. With Tiny Desk, you can manage bookmarks, create to-do lists, and write notes. Start using Tiny Desk and boost your web browsing productivity today.`,
+        author: `Fabio Calero and Team KGB`,
+        viewport: "width=device-width, initial-scale=1, shrink-to-fit=no",
+        image:
+            "https://ucarecdn.com/b739fa8a-df7e-4df0-8ed5-7f364d6e0206/tinydeskdisplay.png",
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Tiny Desk`,
-        short_name: `tinydesk`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/icon.png`, // This path is relative to the root of the site.
-        icons: [
-          {
-            src: `src/images/icon_128.png`,
-            sizes: `128x128`,
-            type: `image/png`,
-          },
-        ],
-      },
-    },
-    `gatsby-plugin-sass`,
-    {
-      resolve: `gatsby-plugin-material-ui`,
-      options: {
-        stylesProvider: {
-          injectFirst: true,
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
         },
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    `gatsby-plugin-offline`,
-  ],
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `Tiny Desk`,
+                short_name: `tinydesk`,
+                start_url: `/`,
+                background_color: `#663399`,
+                theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: `src/images/icon.png`, // This path is relative to the root of the site.
+                icons: [
+                    {
+                        src: `src/images/icon_128.png`,
+                        sizes: `128x128`,
+                        type: `image/png`,
+                    },
+                ],
+            },
+        },
+        `gatsby-plugin-sass`,
+        {
+            resolve: `gatsby-plugin-material-ui`,
+            options: {
+                stylesProvider: {
+                    injectFirst: true,
+                },
+            },
+        },
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        `gatsby-plugin-offline`,
+    ],
 };
